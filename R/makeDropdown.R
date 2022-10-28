78a8f89d12fb162d7188c9d0cc57e04559dcf828
embertokarski <112200247+embertokarski@users.noreply.github.com>
2022-10-28 10:11:42 -0400

    Fix to correctly create visibility list.

--- R/makeDropdown.R
+++ R/makeDropdown.R
@@ -21,18 +21,26 @@ makeDropdown <- function(cols, shared) {
   message(ncols, " candidate grouping", ifelse(ncols > 1, "s", ""))
 
   # keep track of visibility for each associated trace
-  vis <- lapply(cols, function(x) return(FALSE))
+  vis_levels <- list()
+  #improve loop
+  for (column in cols){
+    vis_levels <- append(vis_levels, nlevels(factor(shared$data()[[column]])))
+  }
+  
+  names(vis_levels) <- cols
+  vis <- as.list(rep(FALSE, Reduce('+', vis_levels)))
+  ###########
+  
   vis_start <- 1
-
   # create & populate menu
   dropdown_list <- list()
   for (column in cols) { 
     
     # visibility
     vis[] <- FALSE
-    group <- factor(shared$data()[[column]])
-    vis[seq(vis_start, vis_start + nlevels(group) - 1)] <- TRUE
-    vis_start <- vis_start + nlevels(group)
+    group <- as.numeric(vis_levels[column]) 
+    vis[seq(vis_start, vis_start + (group) - 1)] <- TRUE
+    vis_start <- vis_start + group
     
     # add this new entry to dropdown_list 
     dropdown_list <- append(dropdown_list, 
